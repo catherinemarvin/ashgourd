7778560b8b2f1328bcaac2c63fa20ed92fad1426
Kevin Hwang <k3vinhwang@gmail.com>
2012-07-01 19:06:33 -0700

    server-side client detection

--- server.js
+++ server.js
@@ -12,8 +12,28 @@ server.set("view engine", "ejs");
 server.set("views", __dirname + "/views");
 server.use("/static", express.static(__dirname + "/static"));
 
+/*
+var agent = navigator.userAgent.toLowerCase();
+			var usePrivateView = ((agent.indexOf('iphone') != -1) || (agent.indexOf("android") != -1));
+			if (usePrivateView) {
+				alert("view your hand");
+			} else {
+				alert('view the board');
+			}
+			*/
+
+var checkMobile = function (userAgent) {
+	var ua = userAgent.toLowerCase();
+	return ((ua.indexOf('iphone') != -1) || (ua.indexOf('android') != -1));
+}
+
 server.get("/", function (req, res) {
-	res.render("index");
+	var usePrivateView = checkMobile(req.headers['user-agent']);
+	if (usePrivateView) {
+		res.render("private");
+	} else {
+		res.render("board");
+	}
 });
 
 server.listen(80);
